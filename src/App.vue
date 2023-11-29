ba507452acfa976e6ad293a0116b8875909130ba
sharif <shahanur700@gmail.com>
2023-11-29 23:16:26 +0600

    emit in progress

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,19 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import {reactive, ref} from "vue";
+import DropDown from "@/components/DropDown.vue";
+const items=reactive([1, 2, 3])
+const selected = ref(null)
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
   </header>
 
   <main>
-
+    <DropDown
+        :items="items"
+        v-model="selected"
+        multi-select
+    />
   </main>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
--- src/components/DropDown.vue
+++ src/components/DropDown.vue
@@ -1,11 +1,91 @@
 <script setup>
+import {ref} from "vue";
+import {watch} from "vue";
 
+const props = defineProps({
+  items: {
+    type: Array,
+    required: true
+  },
+  multiSelect: {
+    type: Boolean,
+    default: false,
+    required: false
+  },
+  modelValue:{
+    type:Array,
+  }
+})
+
+const emit = defineEmits(['update:modelValue'])
+
+const showList = ref('none')
+
+/*
+* array that store selected item from dropdown
+*/
+const selectedItems = ref([])
+
+// watch(()=>selectedItems, (newVal, oldVal) => {
+//   console.log('service changed', newVal, oldVal);
+// }, { deep: true });
+
+/**
+ * toggle selected item inside array
+ */
+const addItems = (item) => {
+  if (item && props.multiSelect)
+    addItemForMultiSelect(item)
+  else
+    addItemForSingleSelect(item)
+
+  emit('update:modelValue', selectedItems.value)
+}
+
+const addItemForMultiSelect = (item) => {
+  if (item && !selectedItems.value.includes(item))
+    selectedItems.value.push(item)
+  else
+    selectedItems.value.splice(selectedItems.value.indexOf(item), 1)
+}
+
+const addItemForSingleSelect = (item) => {
+  selectedItems.value.splice(0, selectedItems.value.length)
+  selectedItems.value.push(item)
+}
 </script>
-
 <template>
+  <p v-for="selected in selectedItems">
+    {{ selected }}
+  </p>
+  <input
+      readonly
+      type="text"
+      :value="selectedItems"
+  >
+  <ul
+  >
+    <li
+        :key="index"
+        @click="addItems(item)"
+        @select="addItems(item)"
+        v-for="(item, index) in props.items"
+    >
+      {{ item }}
+      <input
 
+          type="checkbox"
+          :value="item"
+          :checked="selectedItems.includes(item)"
+          v-if="multiSelect">
+    </li>
+  </ul>
 </template>
 
-<style scoped>
-
+<style scoped lang="scss">
+ul {
+  li {
+    cursor: pointer;
+  }
+}
 </style>
